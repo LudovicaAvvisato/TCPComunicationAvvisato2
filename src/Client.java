90f52da791eed9b545b78c4ad469a9f82540aa7b
JazminneC <jazminne2127@gmail.com>
2026-02-18 11:42:10 +0100

    Es TCP avvisato

---
+++ src/Client.java
@@ -0,0 +1,40 @@
+import java.net.Socket;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+
+        try {
+            Socket socket = new Socket("localhost", 3000);
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter pw= new PrintWriter(outputStream);
+            pw.print("Connected to server");
+            pw.flush();
+        }
+        catch (IOException e){
+            System.out.println("Porta non disponibile");
+
+        }
+       /* InputStream inputStream = socket.getInputStream();
+
+
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        String clientMessage = "Ciao";
+        dataOutputStream.writeUTF(clientMessage);
+
+        String serverResponse = dataInputStream.readUTF();
+        System.out.println("Received from server: " + serverResponse);
+
+        inputStream.close();
+        outputStream.close();
+        socket.close();
+        */
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,50 @@
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Server inizio esecuzione");
+
+
+
+        try {
+            ServerSocket server=new ServerSocket(3000);
+            System.out.println("Server in attesa di richieste dal client");
+            Socket s=server.accept();
+            System.out.println("Server connesso");
+        }catch (IOException e){
+            System.out.println("errore");
+        }
+
+        /*int port = 3333;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server connesso alla porta: " + port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket);
+
+            InputStream inputStream = clientSocket.getInputStream(); //LETTURA
+            OutputStream outputStream = clientSocket.getOutputStream(); //SCRITTURA
+
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+            String clientMessage = dataInputStream.readUTF();
+            System.out.println("Ricevuto dal Client: " + clientMessage);
+
+            String serverResponse = "Risposta dal Server: " + clientMessage.toUpperCase();
+            dataOutputStream.writeUTF(serverResponse);
+
+            inputStream.close();
+            outputStream.close();
+            clientSocket.close();
+        }
+        */
+    }
+}
+
