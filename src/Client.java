dbec2a6085dbddf4e37e3a4f41e15e295dbc639c
JazminneC <jazminne2127@gmail.com>
2026-02-23 12:04:49 +0100

    Es TCP avvisato

--- src/Client.java
+++ src/Client.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) throws IOException {
 
-//
+
         try {
             Socket socket = new Socket("localhost", 3000);
             OutputStream outputStream = socket.getOutputStream();
